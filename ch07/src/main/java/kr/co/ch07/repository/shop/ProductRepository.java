f5d21bd7a387dd19e82fd4240ad051229ccc7186
kkj80911 <kkj80911@gmail.com>
2024-03-12 17:49:41 +0900

    Chapter07 추가

---
+++ ch07/src/main/java/kr/co/ch07/repository/shop/ProductRepository.java
@@ -0,0 +1,9 @@
+package kr.co.ch07.repository.shop;
+
+import kr.co.ch07.entity.shop.Product;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface ProductRepository extends JpaRepository<Product, Integer> {
+}
