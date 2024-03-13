817a019775598923b5ce7cfb5c2703076d16c462
kkj80911 <kkj80911@gmail.com>
2024-03-13 17:46:15 +0900

    Chapter08 추가

---
+++ ch07/src/main/java/kr/co/ch07/repository/User4Repository.java
@@ -0,0 +1,4 @@
+package kr.co.ch07.repository.shop;
+
+public class User4Repository {
+}
---
+++ ch08/src/main/java/kr/co/ch08/dto/UserDTO.java
@@ -0,0 +1,31 @@
+package kr.co.ch08.dto;
+
+import kr.co.ch08.entity.User;
+import lombok.Builder;
+import lombok.Data;
+
+import java.time.LocalDateTime;
+
+@Data @Builder
+public class UserDTO {
+
+    private String uid;
+    private String pass;
+    private String name;
+    private int age;
+    private String hp;
+    private String role;
+    private LocalDateTime regDate;
+
+    public User toEntity(){
+            return User.builder()
+                    .uid(uid)
+                    .pass(pass)
+                    .name(name)
+                    .age(age)
+                    .hp(hp)
+                    .role(role)
+                    .regDate(regDate).build();
+    }
+}
+
