7adbcdd5309e5bd140c6447f52cf0c7acb572f05
sun-gwang <142211574+sun-gwang@users.noreply.github.com>
2024-03-19 23:18:32 +0900

    add

--- sboard/src/main/java/kr/co/sboard/config/RootConfig.java
+++ sboard/src/main/java/kr/co/sboard/config/RootConfig.java
@@ -1,8 +1,6 @@
 package kr.co.sboard.config;
 
 import org.modelmapper.ModelMapper;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.info.BuildProperties;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
